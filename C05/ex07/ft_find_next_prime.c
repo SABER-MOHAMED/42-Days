835dc9002719def528b347ecbf0f62207ece3f6e
Mohamed Saber <msaber@e2r7p1.1337.ma>
2022-08-07 10:13:46 +0100

    optimisation de find_next_prime function

--- C05/ex07/ft_find_next_prime.c
+++ C05/ex07/ft_find_next_prime.c
@@ -6,26 +6,28 @@
 /*   By: msaber <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/03 09:58:22 by msaber            #+#    #+#             */
-/*   Updated: 2022/08/03 11:47:26 by msaber           ###   ########.fr       */
+/*   Updated: 2022/08/07 09:30:43 by msaber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-int	ft_find_next_prime(int nb)
+int	ft_num_is_prime(int nb)
 {
-	int	i;
-	int	j;
-
-	i = nb;
-	if (nb <= 0)
+	int i;
+	if (nb <= 2)
 		return (0);
-	while (i >= nb && (i * i <= 2147483647))
+	if (nb >= 2147483630 && nb <= 2147483647)
+		return (2147483647);
+	i = 2;
+	while (i <= nb / 2)
 	{
-		j = 2;
-		while ((i % j) != 0 && (j * j <= i))
-			j++;
-		if ((j * j) > i)
-			return (i);
+		if (nb % i == 0)
+			return (0);
 		i++;
 	}
-	return (0);
+	return (1);
+}
+int ft_find_next_prime(int nb)
+{
+	while (! ft_num_is_prime(nb))
+		nb++;
+	return (nb);
 }
